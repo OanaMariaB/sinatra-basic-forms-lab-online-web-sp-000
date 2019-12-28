726b8df458991ed2621eefcaf9f9f98055b0076f
Oana Berentan <oana.berentan@yahoo.com>
2019-12-28 20:49:07 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,10 +2,10 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  def initialize
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
-    @puppyage = puppyage
+    @age = age
   end
 
 end
